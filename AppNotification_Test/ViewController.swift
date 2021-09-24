fd687a6c94a3a2753cd69067ae2f21efe6ac7b76
SMizzz <shinn.mizzz@gmail.com>
2021-09-24 17:08:02 +0900

    git init

---
+++ AppNotification_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotification_Test
+//
+//  Created by 신미지 on 2021/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
